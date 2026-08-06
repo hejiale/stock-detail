e4c4247926e7af6fc54d36eb4c447522dcfec06e
hejiale <823818902@qq.com>
2026-08-06 17:25:40 +0800

    feat: 修改动态电价获取币种接口

--- data.js
+++ data.js
@@ -110,6 +110,46 @@ window.FUND_HOLDINGS = {
       { name: "康宁", code: "GLW", market: 106, ratio: 3.35 }
     ]
   },
+  huabao: {
+    id: "huabao",
+    name: "华宝",
+    fundName: "华宝致远混合(QDII)C",
+    fundCode: "008254",
+    market: "CN",
+    top10Total: 42.72,
+    holdings: [
+      { name: "闪迪", code: "SNDK", market: 105, ratio: 9.12 },
+      { name: "美光科技", code: "MU", market: 105, ratio: 8.46 },
+      { name: "拉姆研究", code: "LRCX", market: 105, ratio: 3.67 },
+      { name: "台积电", code: "TSM", market: 106, ratio: 3.65 },
+      { name: "MKS", code: "MKSI", market: 105, ratio: 3.61 },
+      { name: "应用材料", code: "AMAT", market: 105, ratio: 3.53 },
+      { name: "阿斯麦", code: "ASML", market: 105, ratio: 3.47 },
+      { name: "超威半导体", code: "AMD", market: 105, ratio: 2.43 },
+      { name: "Nebius", code: "NBIS", market: 105, ratio: 2.41 },
+      { name: "英伟达", code: "NVDA", market: 105, ratio: 2.37 }
+    ]
+  },
+  fuguo: {
+    id: "fuguo",
+    name: "富国",
+    fundName: "富国全球科技互联网股票(QDII)C",
+    fundCode: "022184",
+    market: "CN",
+    top10Total: 47.46,
+    holdings: [
+      { name: "ASMPT", code: "00522", market: 116, ratio: 5.62 },
+      { name: "兆易创新", code: "603986", market: 1, ratio: 5.51 },
+      { name: "闪迪", code: "SNDK", market: 105, ratio: 5.04 },
+      { name: "生益科技", code: "600183", market: 1, ratio: 4.7 },
+      { name: "海力士", code: "000660", market: 177, ratio: 4.62 },
+      { name: "三星电子", code: "005930", market: 177, ratio: 4.58 },
+      { name: "铠侠", code: "285A", market: 176, ratio: 4.58 },
+      { name: "美光科技", code: "MU", market: 105, ratio: 4.31 },
+      { name: "希捷科技", code: "STX", market: 105, ratio: 4.28 },
+      { name: "西部数据", code: "WDC", market: 105, ratio: 4.22 }
+    ]
+  },
   cnSemi: {
     id: "cnSemi",
     name: "A股",
